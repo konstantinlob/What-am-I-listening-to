eef7dcf542e7a160d358c802cc41d5d491ce45b3
roriwa <robinrwalter2002@gmail.com>
2022-09-08 13:43:03 +0200

    adjusted nuxt for gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,11 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    target: 'static',
+    app: {
+      baseURL: "/What-am-I-listening-to/",
+      buildAssetsDir: "nuxt"  // default: _nuxt
+    },
     css: ["@/assets/css/tailwind.css"],
     build: {
       postcss: {
