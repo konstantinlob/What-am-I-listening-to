a8cdb31e7b997105a95a0981044a31c6e1dc6cc0
roriwa <robinrwalter2002@gmail.com>
2022-09-08 12:42:16 +0200

    Updates

---
+++ _nuxt/index.4397d2b8.js
@@ -0,0 +1,1 @@
+import{a as e,o,b as t}from"./entry.44189437.js";const n={},c={class:"text-spotify-green underline"};function r(s,a){return o(),t("p",c,"Hello World")}const l=e(n,[["render",r]]);export{l as default};
