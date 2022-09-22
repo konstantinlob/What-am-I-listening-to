ddca4c150ee5b6db5c6ee7dd871e13266066429a
roriwa <robinrwalter2002@gmail.com>
2022-09-22 13:20:22 +0200

    added ; and code reformat

--- pages/login.vue
+++ pages/login.vue
@@ -1,53 +1,56 @@
 <script setup>
-    // import { generateCodeVerifier,  generateChallenge } from 'pkce-challenge'
-    import pkceChallenge from 'pkce-challenge'
+    import pkceChallenge from 'pkce-challenge';
 
-    const client_id = "20aa48c2719e42c0be5f3b834942f06d"
-    const scopes = []  // https://developer.spotify.com/documentation/general/guides/authorization/scopes/
-    const redirect_uri = new URL(window.location)
-    redirect_uri.search = ""  // clear search stuff
-    const auth_token = localStorage.getItem('auth-token')
+    const client_id = "20aa48c2719e42c0be5f3b834942f06d";
+    const scopes = [];  // https://developer.spotify.com/documentation/general/guides/authorization/scopes/
+    const redirect_uri = new URL(window.location);  // use the current location
+    redirect_uri.search = "";  // clear query parameters
+    const auth_token = localStorage.getItem('auth-token');
 
     async function logout(){
-        localStorage.removeItem('auth-token')
-        navigateTo('/')
+        localStorage.removeItem('auth-token');
+        navigateTo('/');
     }
 
     async function login(){
-        const pkce = pkceChallenge()
+        const pkce = pkceChallenge();
 
-        localStorage.setItem('code-verifier', pkce.code_verifier)
+        // save for pkce post-request
+        localStorage.setItem('code-verifier', pkce.code_verifier);
 
-        const url = new URL('/authorize', 'https://accounts.spotify.com/')
-        url.searchParams.append('client_id', client_id)
-        url.searchParams.append('response_type', 'code')
-        url.searchParams.append('redirect_uri', redirect_uri)
-        url.searchParams.append('scopes', scopes.join(" "))
-        url.searchParams.append('code_challenge_method', 'S256')
-        url.searchParams.append('code_challenge', pkce.code_challenge)
-        navigateTo(url.toString(), {external: true})
+        const url = new URL('/authorize', 'https://accounts.spotify.com/');
+        url.searchParams.append('client_id', client_id);
+        url.searchParams.append('response_type', 'code');
+        url.searchParams.append('redirect_uri', redirect_uri);
+        url.searchParams.append('scopes', scopes.join(" "));
+        url.searchParams.append('code_challenge_method', 'S256');
+        url.searchParams.append('code_challenge', pkce.code_challenge);
+        navigateTo(url.toString(), {external: true});  // page redirect
     }
 
     async function loadToken(){
-        const code_verifier = localStorage.getItem('code-verifier')
+        // generated in login()
+        const code_verifier = localStorage.getItem('code-verifier');
 
-        const url = new URL('/api/token', 'https://accounts.spotify.com/')
-        const form = new FormData()
-        form.append('grant_type', "authorization_code")
-        form.append('code', pkce)
-        form.append('redirect_uri', redirect_uri)
-        form.append('client_id', client_id)
-        form.append('code_verifier', code_verifier)
+        const url = new URL('/api/token', 'https://accounts.spotify.com/');
+
+        const form = {
+            grant_type: "authorization_code",
+            code: pkce,
+            redirect_uri: redirect_uri,
+            client_id: client_id,
+            code_verifier: code_verifier
+        };
         
         const response = await fetch(url, {
             method: "POST",
-            body: new URLSearchParams(form),  // don't ask. i have no idea
-            // headers: {
-            //     "Content-Type": "application/x-www-form-urlencoded"
-            // }
-        })
+            body: new URLSearchParams(form),
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+        });
 
-        const data = await response.json()
+        const data = await response.json();
         // {
         // "access_token": "NgCXRK...MzYjw",
         // "token_type": "Bearer",
@@ -56,29 +59,24 @@
         // "refresh_token": "NgAagA...Um_SHo"
         // }
 
-        console.log(data)
-
         if(data.error){
-            const url = new URL(redirect_uri)
-            url.searchParams.append('error', data.error)
-            navigateTo(url)
+            const url = new URL(redirect_uri);
+            url.searchParams.append('error', data.error);
+            navigateTo(url);
         }else{
-            localStorage.setItem('auth-token', data.access_token)
-            localStorage.setItem('refresh-token', data.refresh_token)
-            localStorage.removeItem('code-verifier')
-            navigateTo('/')
+            localStorage.setItem('auth-token', data.access_token);
+            localStorage.setItem('refresh-token', data.refresh_token);
+            localStorage.setItem('expire-time', Date.now() + data.expires_in);  // timestamp
+            localStorage.removeItem('code-verifier');
+            navigateTo('/');
         }
     }
 
-    function clearCodeVerifier(){
-        localStorage.removeItem('code-verifier')
-    }
-
-    const params = new URLSearchParams(window.location.search)
-    const error = params.get('error')
-    const pkce = params.get('code')
+    const params = new URLSearchParams(window.location.search);
+    const error = params.get('error');
+    const pkce = params.get('code');
     if(pkce){
-        loadToken()
+        await loadToken();  // wait until loaded
     }
 </script>
 
@@ -100,7 +98,5 @@
                 <button class="text-spotify-green underline" @click="login">Login with Spotify</button>
             </div>
         </div>
-
-        <button @click="clearCodeVerifier">Clear Code Verifier</button>
     </div>
 </template>
