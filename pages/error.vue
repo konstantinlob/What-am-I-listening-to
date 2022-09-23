f8fc75e7d395884194ea8c5728aa4b4b9b061559
Julius Jänchen <julius.jaenchen@check24.de>
2022-09-23 18:23:51 +0200

    corrected error page bugs

--- pages/error.vue
+++ pages/error.vue
@@ -1,6 +1,6 @@
-<script>
+<script setup>
     function goBack() {
-        let params = new URLSearchParams(window.location.href);
+        let params = new URLSearchParams(window.location.search);
         navigateTo(params.get("redirect-uri"));
     }
 </script>
@@ -10,6 +10,6 @@
     <div class="flex flex-col items-center">
         <img src="~/assets/gif/burning-room.gif" alt="burning room"/>
         <h1>Oops! Somthing went wrong</h1>
-        <div @click="goBack" class="bg-purple p-3 mt-10 rounded-md">back</div>
+        <div @click="goBack()" class="bg-purple p-3 mt-10 rounded-md">back</div>
     </div>
 </template>
