59925d5ce121d0060172b6fd819fb3eb94f3c60b
Julius Jänchen <julius.jaenchen@check24.de>
2022-09-26 14:26:59 +0200

    Updates

--- nuxt/index.73733b10.js
+++
@@ -1,1 +0,0 @@
-import{e,o as t,a}from"./entry.fbdd494c.js";const s={__name:"index",setup(o){return localStorage.getItem("auth-token")?e("/test"):e("/login"),(n,l)=>(t(),a("p",null,"..."))}};export{s as default};
---
+++ nuxt/index.9406e8e9.js
@@ -0,0 +1,1 @@
+import{a as t,b as e}from"./entry.97438936.js";const n=t({__name:"index",setup(a){return localStorage.getItem("auth-token")?e("/test"):e("/login"),()=>{}}});export{n as default};
