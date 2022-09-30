ea2237bf767b494dd4e89db5f8edc1913bf0ff11
Julius Jänchen <julius.jaenchen@check24.de>
2022-09-30 14:37:21 +0200

    simplified code

--- components/TimeframeSelection/TimeframeSelection.vue
+++ components/TimeframeSelection/TimeframeSelection.vue
@@ -3,55 +3,35 @@
         <span 
             class="cursor-pointer relative hidden-green-underline hover:text-spotify-green transition-colors" 
             :class="[activeTimeframe === Timeframe.Month ? 'before:w-full !text-white' : 'before:w-0']"
-            @click="setActiveTimeframe(Timeframe.Month)" 
+            @click="activeTimeframe = Timeframe.Month" 
         >
             last month
         </span>
         <span class="cursor-pointer relative hidden-green-underline hover:text-spotify-green transition-colors" 
             :class="[activeTimeframe === Timeframe.Year ? 'before:w-full !text-white' : 'before:w-0']"
-            @click="setActiveTimeframe(Timeframe.Year)" 
+            @click="activeTimeframe = Timeframe.Year" 
         >
             last year
         </span>
         <span class="cursor-pointer relative hidden-green-underline hover:text-spotify-green transition-colors" 
             :class="[activeTimeframe === Timeframe.AllTime ? 'before:w-full !text-white' : 'before:w-0']"
-            @click="setActiveTimeframe(Timeframe.AllTime)" 
+            @click="activeTimeframe = Timeframe.AllTime" 
         >
             all time
         </span>
     </div>
 </template>
 
-<script lang="ts" scoped>
+<script lang="ts" setup>
     enum Timeframe {
         Month,
         Year,
         AllTime,
     }
-
-    let activeTimeframe = ref(Timeframe.Year);
-    const setActiveTimeframe = (activeTimeframeInput: Timeframe) => {
-        console.log(activeTimeframe);
-        activeTimeframe.value = activeTimeframeInput;
-    }
-
-    export default {
-        expose: ["activeTimeframe", "Timeframe"],
-        data() {
-            return {
-                Timeframe,
-                activeTimeframe,  
-            }
-        },
-        methods: {
-            setActiveTimeframe,
-        },
-    }
+    const activeTimeframe = ref(Timeframe.Year);
 </script>
 
 <style scoped>
-/*hover:before:w-full*/
-
 .hidden-green-underline {
     @apply before:h-1 before:rounded-md before:absolute before:-bottom-1 before:bg-spotify-green before:transition-all
 }
