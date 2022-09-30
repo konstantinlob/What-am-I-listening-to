4b2ddb6b37fab635abd78bfd77fbc6e2d7af7879
roriwa <robinrwalter2002@gmail.com>
2022-09-30 14:38:11 +0200

    added new ts asses to manage requests to the api

---
+++ assets/ts/api/request.ts
@@ -0,0 +1,37 @@
+interface requestParameter {
+    endpoint: string,
+    query?: object,
+    body?: object,
+    method?: 'GET' | 'POST' | 'PUT',
+    headers?: object
+}
+
+
+export async function request<dataType>({endpoint, query, body, method, headers}: requestParameter): Promise<dataType> {
+    const auth = localStorage.getItem('auth-token');
+    if(!auth){
+        throw Error('not logged in')
+    }
+
+    const url = new URL(`https://api.spotify.com/v1${endpoint}`);
+    if(query){
+        for(let [key, value] of Object.entries(query)){
+            url.searchParams.append(key, value)
+        }
+    }
+
+    return fetch(url, {
+        method: method ?? 'GET',
+        headers: {
+            'Authorization': `Bearer ${auth}`,
+            'Content-Type': 'application/json',
+            ...headers
+        },
+        body: body ? JSON.stringify(body) : undefined,
+    }).then(response => {
+        if(!response.ok){
+            throw Error(`${endpoint} failed with ${response.status}`)
+        }
+        return response.json()
+    })
+}
