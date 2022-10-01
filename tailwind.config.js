62d4a1c2f1e0e84f66d3a43802c2ffc0d0d2ee0e
Markus Geilehner <markus@geilehner.io>
2022-10-01 15:51:30 +0200

    added introduction page

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,14 @@ module.exports = {
   theme: {
     extend: {},
     colors: {
+      gray: "#4F4F4F",
       "white": "#FFFFFF",
       "spotify-green": "#1DB954",
       "purple": "#6D3CB4",
       "spotify-black": "#191414",
     },
     borderRadius: {
+      "full": "999px",
       "lg": "20px",
       "md": "10px",
     },
