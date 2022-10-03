728dfc75e2de5e697d845e2a9ae472242c6be338
roriwa <robinrwalter2002@gmail.com>
2022-10-03 21:05:14 +0200

    added comment and extended check

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <script lang="ts" setup>
     if(!localStorage.getItem('auth-token')){
-        if(localStorage.getItem('introduced')){
+        if(localStorage.getItem('introduced') !== null){
             navigateTo("/login");
         } else {
+            // 'true' is just for the human. it doesn't matter what is saved
             localStorage.setItem('introduced', "true");  // maybe move to end of /introduction 
             navigateTo("/introduction");
         }
