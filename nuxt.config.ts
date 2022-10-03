ca30db7583be1ea4d8fa4eab2771b925615c6c32
Julius Jänchen <julius.jaenchen@check24.de>
2022-10-03 11:40:15 +0200

    replaced tailwindcss with nuxtjs/tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,3 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
     target: 'static',
@@ -8,16 +6,5 @@ export default defineNuxtConfig({
       baseURL: "/What-am-I-listening-to/",  // repo name to build correctly for github-pages
       buildAssetsDir: "nuxt"  // default: _nuxt // _nuxt is not accepted by github-pages
     },
-    css: ["@/assets/css/tailwind.css"],
-    build: {
-      postcss: {
-        postcssOptions: {
-          plugins: {
-            tailwindcss: {},
-            autoprefixer: {},
-          },
-        },
-      },
-    },
-    modules: ["tailwindcss"],
+    modules: ["@nuxtjs/tailwindcss"],
 })
