a666654a2a43c62c9c8e519f751a701c6fff5841
Hrushikesh Thorat <hrushikeshst54@gmail.com>
2022-10-04 14:37:36 +0530

    Added: vite-svg-loader path

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,7 @@
 <script lang="ts" setup>
     import pkceChallenge from "pkce-challenge";
+    import GithubBlack from "~/assets/svg/Github_Logo_Black.svg?component"
+    import SpotifyWhite from "~/assets/svg/Spotify_Logo_White.svg"
 
     if (localStorage.getItem("auth-token") !== null) {
         navigateTo("/home");
@@ -61,7 +63,7 @@
             </div>
             <div class="flex flex-col items-center">
                 <button class="bg-spotify-green px-8 py-4 h-min rounded-lg flex flex-row items-center font-bold" @click="login()">
-                    <img src="~/assets/svg/Spotify_Logo_White.svg" alt="Spotify Logo" class="h-10">
+                    <SpotifyWhite class="h-10 w-full"/>
                     <span class="pl-2 text-[25px]">login</span>
                 </button>
                 <span class="flex items-center">Secured by OAuth2.0</span>
@@ -69,7 +71,7 @@
         </div>
         <div class="flex flex-col items-center">
             <button class="bg-purple px-4 py-2 h-min rounded-lg flex flex-row items-center font-bold">
-                <img src="~/assets/svg/Github_Logo_Black.svg" alt="GitHub Logo" class="h-8">
+                <GithubBlack class="h-8 w-6"/>
                 <span class="pl-2 text-[20px]">GitHub repo</span>
             </button>
             <span class="text-center w-full">Developled by <br> Benjamin Tallarek, Julius Jänchen, Konstantin Lobmayr, Pascal Heyn, Robin Walter</span>
