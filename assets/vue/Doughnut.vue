90892f54022fa33f159efeb915e9bd0682cba0a2
Julius Jänchen <julius.jaenchen@check24.de>
2022-10-06 23:11:43 +0200

    added placeholder doughnut with chartjs

---
+++ assets/vue/Doughnut.vue
@@ -0,0 +1,36 @@
+<template>
+    <Doughnut
+        :chart-data="chartData"
+        :chart-options="chartOptions"
+    />
+</template>
+
+<script lang="ts" setup>
+    import { Doughnut } from "vue-chartjs";
+    import { Chart as ChartJS, registerables } from "chart.js";
+    ChartJS.register(...registerables);
+
+    const chartData = {
+        labels: ["VueJs", "EmberJs", "ReactJs", "AngularJs"],
+        datasets: [
+            {
+                backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16"],
+                data: [40, 20, 80, 10],
+            },
+        ],
+    };
+
+    const chartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+            legend: {
+                position: "bottom",
+                labels: {
+                    color: "white",
+                },
+            },
+        },
+    };
+
+</script>
--- components/Statistics/GreenPlaceholder.vue
+++ components/Statistics/GreenPlaceholder.vue
@@ -1,44 +1,9 @@
 <template>
     <section class="w-full h-full flex justify-center items-center">
-        <div class="relative w-[200px] h-[200px]">
-            <template v-for="genre in genres" :key="genre.name">
-                <div
-                    class="absolute-center w-full h-full rounded-full"
-                    :style="{
-                        'background': `conic-gradient(rgba(0,0,0,0) ${genre.startingPosition}turn, ${genre.color} ${genre.startingPosition}turn ${genre.startingPosition + genre.percent}turn, rgba(0,0,0,0) ${genre.startingPosition + genre.percent}turn 1turn)`,
-                        }"
-                ></div>
-            </template>
-        </div>
+        <Doughnut />
     </section>
 </template>
 
 <script lang="ts" setup>
-
-    interface Genre {
-        name: string,
-        color: string,
-        percent: number,
-        startingPosition: number,
-    };
-    const genres: Array<Genre> = [
-        {
-            name: "Hip-hop",
-            color: "#eb3d34",
-            percent: 0.3333,
-            startingPosition: 0,
-        },
-        {
-            name: "Rock",
-            color: "#34d9eb",
-            percent: 0.6666,
-            startingPosition: 0.3333,
-        },
-    ];
+    import Doughnut from "~/assets/vue/Doughnut.vue";
 </script>
-
-<style scoped>
-    .absolute-center{
-        @apply absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2
-    }
-</style>
