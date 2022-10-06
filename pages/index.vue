207b6739a7273228bfc14075e939f41dcea20e08
roriwa <103991412+roriwa@users.noreply.github.com>
2022-10-06 16:51:52 +0200

    removed comments

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,7 @@
         if(localStorage.getItem('introduced') !== null){
             navigateTo("/login");
         } else {
-            // 'true' is just for the human. it doesn't matter what is saved
-            localStorage.setItem('introduced', "true");  // maybe move to end of /introduction 
+            localStorage.setItem('introduced', "true");
             navigateTo("/introduction");
         }
     } else {
