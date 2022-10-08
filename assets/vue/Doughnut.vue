dbd25168ae2f21e004cf955d1f77666986abd218
Julius Jänchen <julius.jaenchen@check24.de>
2022-10-08 16:49:19 +0200

    added genre Prop

--- assets/vue/Doughnut.vue
+++ assets/vue/Doughnut.vue
@@ -7,16 +7,18 @@
     import { Chart as ChartJS, registerables } from "chart.js";
     ChartJS.register(...registerables);
 
-    const chartData = {
-        labels: ["VueJs", "EmberJs", "ReactJs", "AngularJs"],
-        datasets: [
-            {
-                backgroundColor: ["#41B883", "#E46651", "#00D8FF", "#DD1B16"],
-                borderColor: "#191414",
-                data: [40, 20, 80, 10],
-            },
-        ],
-    };
+    interface Genre {
+        name: string,
+        count: number,
+    }
+    interface Props {
+        genres: Genre[],
+    }
+    const props = withDefaults(defineProps<Props>(), {
+        genres: () => [{ name: "Missing Data", count: 1 }],
+    });
+
+    const { genres } = toRefs(props);
 
     const chartOptions = {
         responsive: true,
@@ -31,4 +33,32 @@
         },
     };
 
+    // testdata
+    // ["VueJs", "EmberJs", "ReactJs", "AngularJs"]
+    // [40, 20, 80, 10]
+    interface ChartData {
+        labels: string[],
+        datasets: Array<{
+            backgroundColor: string[],
+            borderColor: string,
+            data: number[],
+        }>,
+    }
+
+    const chartData: ChartData = {
+        labels: [],
+        datasets: [
+            {
+                // only 20 distinct background colors
+                backgroundColor: ["#e6194b", "#3cb44b", "#ffe119", "#4363d8", "#f58231", "#911eb4", "#46f0f0", "#f032e6", "#bcf60c", "#fabebe", "#008080", "#e6beff", "#9a6324", "#fffac8", "#800000", "#aaffc3", "#808000", "#ffd8b1", "#000075", "#808080"],
+                borderColor: "#191414",
+                data: [],
+            },
+        ],
+    };
+
+    genres.value.forEach((genre) => {
+        chartData.labels.push(genre.name);
+        chartData.datasets[0].data.push(genre.count);
+    });
 </script>
--- components/Statistics/GreenPlaceholder.vue
+++ components/Statistics/GreenPlaceholder.vue
@@ -1,9 +1,20 @@
 <template>
     <section class="w-full h-full flex justify-center items-center">
-        <Doughnut />
+        <Doughnut :genres="genres" />
     </section>
 </template>
 
 <script lang="ts" setup>
     import Doughnut from "~/assets/vue/Doughnut.vue";
+
+    const genres = [
+        {
+            name: "VueJs",
+            count: 40,
+        },
+        {
+            name: "EmberJs",
+            count: 20,
+        },
+    ];
 </script>
