dc00cacae04d77dcc219a28968623761404051b0
Julius Jänchen <julius.jaenchen@check24.de>
2022-10-09 17:21:02 +0200

    added timeframe support

--- components/Statistics/GenreDoughnut.vue
+++ components/Statistics/GenreDoughnut.vue
@@ -9,14 +9,21 @@
     import Doughnut from "~/assets/vue/Doughnut.vue";
     import { request } from "~/assets/ts/api";
     import { TopArtists } from "~/assets/ts/api/types";
+    import { Timeframe } from "~/assets/ts/enums";
+
+    const activeTimeframe = useState<Timeframe>("activeTimeframe").value;
+    const topArtists = {
+        [Timeframe.Month]: await request<TopArtists>({ endpoint: "/me/top/artists", query: { time_range: "short_term" } }),
+        [Timeframe.HalfYear]: await request<TopArtists>({ endpoint: "/me/top/artists", query: { time_range: "medium_term" } }),
+        [Timeframe.Year]: await request<TopArtists>({ endpoint: "/me/top/artists", query: { time_range: "long_term" } }),
+    };
 
     interface Genre {
         name: string,
         count: number,
     }
     const genres: Genre[] = [];
-    const topArtists = await request<TopArtists>({ endpoint: "/me/top/artists" });
-    topArtists.items.forEach(artist => artist.genres.forEach((genre) => {
+    topArtists[activeTimeframe].items.forEach(artist => artist.genres.forEach((genre) => {
         if (genres.length >= 20) { // due to limited amount of colors
             return;
         }
