ec06b85817cc6f65372ab228e5cc98cb5bfb2631
Benjamin Tallarek / PC <bennitallarek@gmail.com>
2022-10-10 17:05:24 +0200

    changed back a navigateTo in index.vue - was just for easier testin

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
             localStorage.setItem('introduced', "true");
             navigateTo("/introduction");
         } else {
-            navigateTo("/introduction"); //TODO: change to login before merge, is just for testing purposes
+            navigateTo("/login");
         }
     } else {
         navigateTo("/home");
