bf0ea7cea43d0d5c92b034fe6b4435fff9d4fb42
Julius Jänchen <julius.jaenchen@check24.de>
2022-10-14 13:35:14 +0200

    added timeRange enum

--- assets/ts/enums.ts
+++ assets/ts/enums.ts
@@ -3,3 +3,9 @@ export enum Timeframe {
     HalfYear,
     Year,
 };
+
+export const timeRange = {
+    [Timeframe.Month]: "short_term",
+    [Timeframe.HalfYear]: "medium_term",
+    [Timeframe.Year]: "long_term",
+};
