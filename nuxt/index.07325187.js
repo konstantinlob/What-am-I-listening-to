0595f9b6904182fefce03a2e184aece74873f08a
roriwa <robinrwalter2002@gmail.com>
2022-10-15 17:25:16 +0200

    Updates

--- nuxt/index.07325187.js
+++
@@ -1,1 +0,0 @@
-import{a as t,f as e}from"./entry.67fa0ab4.js";const a=t({__name:"index",setup(o){return localStorage.getItem("auth-token")===null?localStorage.getItem("introduced")===null?(localStorage.setItem("introduced","true"),e("/introduction")):e("/login"):e("/home"),()=>{}}});export{a as default};
