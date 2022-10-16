266146f121a8b64042139f70ec9db1d2479407b8
Julius Jänchen <julius.jaenchen@check24.de>
2022-10-16 21:10:57 +0200

    Updates

---
+++ nuxt/logout.f8032a64.js
@@ -0,0 +1,1 @@
+import{f as e}from"./entry.2f26497e.js";const a={__name:"logout",setup(o){return localStorage.removeItem("auth-token"),localStorage.removeItem("refresh-token"),localStorage.removeItem("auth-state"),localStorage.removeItem("code-verifier"),e("/"),()=>{}}};export{a as default};
