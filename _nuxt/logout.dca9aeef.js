e6260f345de9e22dfbb91b5083cf01480b6e576c
roriwa <robinrwalter2002@gmail.com>
2022-10-17 14:14:55 +0200

    Updates

---
+++ _nuxt/logout.dca9aeef.js
@@ -0,0 +1,1 @@
+import{b as e}from"./entry.8360cc41.js";const a={__name:"logout",setup(o){return localStorage.removeItem("auth-token"),localStorage.removeItem("refresh-token"),localStorage.removeItem("auth-state"),localStorage.removeItem("code-verifier"),e("/"),()=>{}}};export{a as default};
