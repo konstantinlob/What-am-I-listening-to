c4382e4eb75d6d402ef80814d03251be0259c840
Julius Jänchen <julius.jaenchen@check24.de>
2022-10-17 12:56:16 +0200

    fixed invalid array size error

--- assets/ts/helpers/normalizeToFixedLength.ts
+++ assets/ts/helpers/normalizeToFixedLength.ts
@@ -1,5 +1,6 @@
 // doesn't mutate input
 export const normalizeToFixedLength = (array: any[], length: number, padding: any = null) => {
-    const buffer = Array(length - array.length).fill(padding);
-    return [...array.slice(0, length), ...buffer];
+    const trimmedArray = array.slice(0, length);
+    const buffer = Array(length - trimmedArray.length).fill(padding);
+    return [...trimmedArray, ...buffer];
 };
